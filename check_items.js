f3991ffbab9e51704cbb2700e67e86125228d74c
BinomialSheep <jose_jinrou@yahoo.co.jp>
2024-10-06 18:22:01 +0900

    りふぁくた

--- check_items.js
+++ check_items.js
@@ -6,6 +6,7 @@ document.addEventListener("readystatechange", () => {
   }
 });
 
+// 全てのCDに「所持」「未所持」情報を追加する
 async function initializeCheckItems() {
   addHighlightStyle();
 
@@ -18,17 +19,20 @@ async function initializeCheckItems() {
   links.forEach((link) => processLink(link, possessionCdList));
 }
 
+// 「未所持」用スタイルを追加する
 function addHighlightStyle() {
   const style = document.createElement("style");
   style.innerHTML = ".highlight { color: red; } ";
   document.head.appendChild(style);
 }
 
+// 1つのCDに対して「所持」「未所持」テキストを追加する
 function processLink(link, possessionCdList) {
   const href = link.getAttribute("href");
   if (!href || !href.startsWith("/cd")) return;
   const parts = link.getAttribute("href").split("/");
-  const cdId = parts[parts.length - 1];
+
+  const cdId = parts[parts.findIndex((part) => part === "cd") + 1];
 
   const originalText = link.textContent;
   const nextText = possessionCdList.includes(cdId)
--- save_items.js
+++ save_items.js
@@ -2,35 +2,41 @@ const MY_POSSESSION_CD_LIST_KEY = "my_possession_cd_list";
 
 document.addEventListener("readystatechange", () => {
   if (document.readyState === "complete" || document.readyState === "interactive") {
-    initializeSaveItem();
+    addaveItemButton();
   }
 });
 
-function initializeSaveItem() {
+// 「所持CDの更新」ボタンをページに追加する
+function addaveItemButton() {
   const h1Element = document.querySelector("#mypage_possession h1");
   if (!h1Element) return;
 
   const button = createButton("所持CDの更新");
   h1Element.insertAdjacentElement("afterend", button);
-  button.addEventListener("click", savePossessionList);
+  button.addEventListener("click", savePossessionCdList);
 }
 
-function createButton(text = "button", floatStyle = "right", marginTop = "10px") {
+// ボタンのテキストやデザインを設定する
+function createButton(text = "button", floatStyle = "right", marginTop = "10px", marginRight = "5px") {
   const button = document.createElement("button");
   button.textContent = text;
   button.style.float = floatStyle;
   button.style.marginTop = marginTop;
+  button.style.marginRight = marginRight;
   return button;
 }
 
-function savePossessionList() {
+// ボタンを押したら所持CDをChromeストレージを更新する
+function savePossessionCdList() {
   const links = document.querySelectorAll("#mypage_possession ul li a");
   const cdList = Array.from(links).map((link) => {
     const parts = link.getAttribute("href").split("/");
-    return parts[parts.length - 1];
+    // NOTE：「my/possession」と「mypage/possession/p4jyn」でindexが異なるので以下で吸収する
+    return parts[parts.findIndex((part) => part === "cd") + 1];
   });
 
   chrome.storage.local.set({ [MY_POSSESSION_CD_LIST_KEY]: cdList }, () => {
-    console.log(`${MY_POSSESSION_CD_LIST_KEY} が保存されました`, cdList);
+    console.log(`${MY_POSSESSION_CD_LIST_KEY} を保存しました`, cdList);
+    alert("所持CDリストを保存しました！");
   });
 }
